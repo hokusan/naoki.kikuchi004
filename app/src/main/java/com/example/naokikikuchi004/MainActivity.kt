cfebbe96fde6a74163aeeec2bc24858adb7d85fb
hokusan <hokusan.naoki@gmail.com>
2020-11-15 22:54:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/naokikikuchi004/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.naokikikuchi004
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var num = 10
+        Log.d("kotlintest" , Integer.toString(num))
+
+        num = 50
+        Log.d("kotlintest" , num.toString())
+    }
+}
